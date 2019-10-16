2affe45c2ad33b42fc1f2fa920706dd1b4c5eabe
Marlon Luiz Custódio <marlon.custodio@outlook.com>
2019-10-16 09:16:10 -0300

    Ajuste envio id usuário

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 import _ from 'lodash'
 
-const user = _.get(window.user, 'id', null)
-
-export default axios.create({
-  baseURL: 'https://tea-backend.herokuapp.com',
-  headers: {
-    user
-  }
+const api = axios.create({
+  baseURL: 'https://tea-backend.herokuapp.com'
 })
+
+api.interceptors.request.use(config => {
+  const user = _.get(window.user, 'id', null)
+  config.headers.user = user
+
+  return config
+}, window.console.log)
+
+export default api
